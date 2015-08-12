97fce09a3a852c1efc2ea723cea31f2718fdc95e
adishjain <adish.jain1995@ymail.com>
2015-08-12 22:31:06 +0530

    Q-9
    Although test cases run good, this one isn't accepted. (YET)

---
+++ Prime Palindromes/primepalindromes.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+bool prime(int);
+bool palin(int);
+int main(){
+long a;
+cin>>a;
+for(int i=a+1;i<1000000+a;i++){
+    //cout<<"Checking palin for : "<<i<<endl;
+        if(palin(i)){
+
+            if(prime(i)){
+                //cout<<"Checking prime for : "<<i<<endl;
+              cout<<i;
+              break;
+            }
+        }
+}
+return 0;
+}
+bool prime(int i){
+    if(i<0) i=-i;
+
+    for(int j=2;j<=i/2;j++){
+
+        if(i%j==0) return 0;
+    }
+return 1;
+}
+bool palin(int i){
+    if(i<0) i=-i;
+    int r=i;
+    int j=0;
+    while(i>0){
+        j*=10;
+        j+=i%10;
+        i=i/10;
+    }
+    if(j==r) return 1;
+    else return 0;
+}
