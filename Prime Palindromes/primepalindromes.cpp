49c75f1941e46674d5943f2509f8280223354d39
adishjain <adish.jain1995@ymail.com>
2015-08-12 23:00:01 +0530

    Q-9
    Logic's correct for sure.. but still wrong.!

--- Prime Palindromes/primepalindromes.cpp
+++ Prime Palindromes/primepalindromes.cpp
@@ -11,7 +11,7 @@ for(int i=a+1;;i++){
 
             if(prime(i)){
                 //cout<<"Checking prime for : "<<i<<endl;
-              cout<<i;
+              cout<<i<<endl;
               break;
             }
         }
@@ -20,7 +20,7 @@ return 0;
 }
 bool prime(int i){
     if(i<0) i=-i;
-
+if(i<2) return 0;
     for(int j=2;j<=i/2;j++){
 
         if(i%j==0) return 0;
